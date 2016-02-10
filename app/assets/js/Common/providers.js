685cfe7d8ceda7ecdf95c65dc198928538faa8c3
JihadMotii-REISys <jihad.motii@reisystems.com>
2016-02-10 15:09:30 -0500

    remove useless code

--- app/assets/js/Common/providers.js
+++ app/assets/js/Common/providers.js
@@ -7,17 +7,12 @@ providers.AuthProvider = [function () {
                 loginUser: function (credentials) {
                     return ApiService.call('userLogin', '', '', {username: credentials.username, password: credentials.password}, 'POST');
                 },
-//                isUserAuthenticated: function ($http, $rootScope) {
                 isUserAuthenticated: function (callBackFnSuccess, callBackFnError) {
                     return ApiService.call('userIsLoggedIn', '', '', {}, 'POST')
                         .then(function (data) {  //  , status, headers, config
 
                             //session valid
                             if (data.loggedIn) {
-//                                $rootScope.authenticated = {
-//                                    "user": data.user,
-//                                    "success": true
-//                                }
                                 SessionFactory.setSession(data.user);
 
                                 if(typeof callBackFnSuccess === 'function') {
@@ -26,10 +21,6 @@ providers.AuthProvider = [function () {
                             }
                             else //session invalid
                             {
-//                                $rootScope.authenticated = {
-//                                    "user": null,
-//                                    "success": false
-//                                };
                                 SessionFactory.destroySession();
 
                                 if(typeof callBackFnError === 'function') {
@@ -37,25 +28,15 @@ providers.AuthProvider = [function () {
                                 }
                             }
                         }, function () {    //  data, status, headers, config
-//                            $rootScope.authenticated = {
-//                                "user": null,
-//                                "success": false
-//                            };
                             SessionFactory.destroySession();
                         });
                 },
-//                logoutUser: function ($http, $rootScope, $location) {
                 logoutUser: function ($location) {
                     return ApiService.call('userLogOut', '', '', {}, 'POST')
                         .then(function (data) {  //  , status, headers, config
                             if (data.success) {
-//                                $rootScope.authenticated = {
-//                                    "user": null,
-//                                    "success": false
-//                                };
-
                                 SessionFactory.destroySession();
-                                
+
                                 $location.path('/');
                             }
                         });
