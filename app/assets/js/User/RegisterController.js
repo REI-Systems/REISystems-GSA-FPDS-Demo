8a41de93283ec7888f5b0878d6bcea4abe77112b
JihadMotii-REISys <jihad.motii@reisystems.com>
2016-02-10 22:17:59 -0500

    integrate user register feature

--- app/assets/js/User/RegisterController.js
+++ app/assets/js/User/RegisterController.js
@@ -1,5 +1,42 @@
 var controllers = controllers || {};
 
-controllers.RegisterController = ['$scope', function($scope){
+controllers.RegisterController = ['$scope', '$location', 'AuthProvider', 'ApiService', function($scope, $location, AuthProvider, ApiService){
+    //verify if user is aythenticated, if yes init scope
+    AuthProvider.isUserAuthenticated(
+    function(){
+        //user connected, redirect to home page
+        $scope.isUserAuth = true;
+        $location.path('/');
+    }, function(){
+        $scope.isUserAuth = false;
+    });
+
+    //Register function
+    $scope.registerUser = function() {
+        if(typeof $scope.user !== 'undefined' && 
+            typeof $scope.user.username !== 'undefined' && 
+            typeof $scope.user.email !== 'undefined' && 
+            typeof $scope.user.password !== 'undefined') {
+
+            var aParams = {
+                'username': $scope.user.username,
+                'email': $scope.user.email,
+                'password': $scope.user.password,
+                'name': $scope.user.name,
+                'preferences': {
+                    'typeDashboard': $scope.user.typeDashboard
+                }
+            };
+
+            //create user
+            ApiService.call('userCreate', '', {}, aParams, 'POST').then(
+                function(data){
+                    console.log(data);
+                }, 
+                function(error){
+                    console.log(error);
+            });
+        }
+    };
     
 }];
