93caec9e29fdcbc1d4efe4976c684b19132d5816
CarlosValle <REI Systems>
2016-03-08 11:22:58 -0500

    Hide/show colunm markup

--- app/frontend/app/directives/SearchBar.js
+++ app/frontend/app/directives/SearchBar.js
@@ -25,6 +25,64 @@
                 scope.row = event.args.row;
               });
             });
+            
+            // Temp
+            $('.list .master.checkbox')
+              .checkbox({
+                // check all children
+                onChecked: function () {
+                  console.log("checked");
+                  var
+                    $childCheckbox = $(this).closest('.checkbox').siblings('.grid').find('.checkbox')
+                    ;
+                  $childCheckbox.checkbox('check');
+                },
+                // uncheck all children
+                onUnchecked: function () {
+                  var
+                    $childCheckbox = $(this).closest('.checkbox').siblings('.grid').find('.checkbox')
+                    ;
+                  $childCheckbox.checkbox('uncheck');
+                }
+              })
+            ;
+
+            $('.list .child.checkbox')
+              .checkbox({
+                // Fire on load to set parent value
+                fireOnInit: true,
+                // Change parent state on each child checkbox change
+                onChange: function () {
+                  var
+                    $listGroup = $(this).closest('.grid'),
+                    $parentCheckbox = $listGroup.closest('.item').children('.checkbox'),
+                    $checkbox = $listGroup.find('.checkbox'),
+                    allChecked = true,
+                    allUnchecked = true
+                    ;
+                  // check to see if all other siblings are checked or unchecked
+                  $checkbox.each(function () {
+                    if ($(this).checkbox('is checked')) {
+                      allUnchecked = false;
+                    }
+                    else {
+                      allChecked = false;
+                    }
+                  });
+                  // set parent checkbox state, but dont trigger its onChange callback
+                  if (allChecked) {
+                    $parentCheckbox.checkbox('set checked');
+                  }
+                  else if (allUnchecked) {
+                    $parentCheckbox.checkbox('set unchecked');
+                  }
+                  else {
+                    $parentCheckbox.checkbox('set indeterminate');
+                  }
+                }
+              })
+            ;
+
 
           });
         },
