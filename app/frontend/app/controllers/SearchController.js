11fc908076e8a5f95396a77ee45b05bf2f763a27
CarlosValle <REI Systems>
2016-03-10 15:47:38 -0500

    Cleaning Search Controller

--- app/frontend/app/controllers/SearchController.js
+++ app/frontend/app/controllers/SearchController.js
@@ -1,4 +1,4 @@
-(function () {
+(function() {
 
   angular.module('app')
     .controller('SearchController', SearchController);
@@ -12,62 +12,21 @@
     vm.numresults = '0';
     vm.updateTableResults = UpdateTableResults;
 
-    AuthProvider.isUserAuthenticated(function () {
+    AuthProvider.isUserAuthenticated(function() {
       vm.user = SessionFactory.getSession().user;
       if (vm.user.preferences.jqxGridState) {
         $scope.$broadcast('loadTableState', vm.user.preferences.jqxGridState);
-        for(var columnName in vm.user.preferences.jqxGridState.columns){
-          if(!vm.user.preferences.jqxGridState.columns[columnName].hidden){
-            $('input[type=checkbox][name='+columnName+']').parent().checkbox('set checked');
+        for (var columnName in vm.user.preferences.jqxGridState.columns) {
+          if (!vm.user.preferences.jqxGridState.columns[columnName].hidden) {
+            $('input[type=checkbox][name=' + columnName + ']').parent().checkbox('set checked');
           }
-        }        
+        }
       }
     });
 
     function UpdateTableResults(sqlClause) {
       $scope.$broadcast('updateTable', sqlClause);
     }
-    
-    //populate filter fields
-    // var aFilterCol = ['contractactiontype', 'agencyid', 'contractingofficeagencyid',
-    //   'maj_agency_cat', 'psc_cat', 'vendorname',
-    //   'pop_state_code', 'localareasetaside'];
-
-    // var aResultFilterCol = {};
-
-    // SearchService.getFieldsFacet(aFilterCol)
-    //   .then(function (response) {
-
-    //     angular.forEach(response, function (content) {
-
-    //       if (content.data && !content.data.error) {
-
-    //         var key = content.data.cols[0];
-
-    //         angular.forEach(content.data.rows, function (row) {
-
-    //           if (aResultFilterCol.hasOwnProperty(key)) {
-    //             aResultFilterCol[key].push(row[0]);
-    //           } else {
-    //             aResultFilterCol[key] = [];
-    //           }
-
-    //         });
-    //       }
-    //     });
-
-    //     vm.filters = {
-    //       agencies: aResultFilterCol.agencyid,
-    //       contractTypes: aResultFilterCol.contractactiontype,
-    //       contractingAgencies: aResultFilterCol.contractingofficeagencyid,
-    //       localAreas: aResultFilterCol.localareasetaside,
-    //       department: aResultFilterCol.maj_agency_cat,
-    //       popStates: aResultFilterCol.pop_state_code,
-    //       psc: aResultFilterCol.psc_cat,
-    //       vendorname: aResultFilterCol.vendorname
-    //     };
-        
-    //   });
 
   }
 
