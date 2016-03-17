9fec1cf4da5ad73145ffb69ec3dc22940df33f8b
CarlosValle <REI Systems>
2016-03-17 17:14:43 -0400

    Adding gulp task to connect to sails data

--- gulpfile.js
+++ gulpfile.js
@@ -3,46 +3,46 @@ var mainBowerFiles = require('main-bower-files');
 var inject = require('gulp-inject');
 var del = require('del');
 var angularFilesort = require('gulp-angular-filesort');
+var webserver = require('gulp-webserver');
 // To use livereload you also need
 // Chrome livereload extention
 // https://chrome.google.com/webstore/detail/livereload/jnihajbhpnppcggbcgedagnkighmdlei?hl=en
-var livereload = require('gulp-livereload');
+//var livereload = require('gulp-livereload');
 
 var paths = {
   temp: 'temp',
   tempVendor: 'temp/vendor',
   tempVendorImages: 'temp/vendor/images',
   tempIndex: 'temp/index.html',
-  
-  jqxImages: ['bower_components/jqwidgets/jqwidgets/styles/images/icon-up.png','bower_components/jqwidgets/jqwidgets/styles/images/icon-down.png','bower_components/jqwidgets/jqwidgets/styles/images/icon-left.png','bower_components/jqwidgets/jqwidgets/styles/images/icon-right.png','bower_components/jqwidgets/jqwidgets/styles/images/loader.gif','bower_components/jqwidgets/jqwidgets/styles/images/sortasc.png','bower_components/jqwidgets/jqwidgets/styles/images/sortdesc.png','bower_components/jqwidgets/jqwidgets/styles/images/sortremove.png','bower_components/jqwidgets/jqwidgets/styles/images/drag.png'],
-  
+
+  jqxImages: ['bower_components/jqwidgets/jqwidgets/styles/images/icon-up.png', 'bower_components/jqwidgets/jqwidgets/styles/images/icon-down.png', 'bower_components/jqwidgets/jqwidgets/styles/images/icon-left.png', 'bower_components/jqwidgets/jqwidgets/styles/images/icon-right.png', 'bower_components/jqwidgets/jqwidgets/styles/images/loader.gif', 'bower_components/jqwidgets/jqwidgets/styles/images/sortasc.png', 'bower_components/jqwidgets/jqwidgets/styles/images/sortdesc.png', 'bower_components/jqwidgets/jqwidgets/styles/images/sortremove.png', 'bower_components/jqwidgets/jqwidgets/styles/images/drag.png'],
+
   index: 'app/index.html',
   appSrc: ['app/**/*', '!app/index.html', '!app/**/*.js'],
   angularFiles: ['app/**/*.js'],
   bowerSrc: 'bower_components/**/*'
 }
 
-gulp.task('default', ['watch']);
+gulp.task('default', ['serve', 'watch']);
 
-gulp.task('watch', ['copyAll','copyImages'], function(){
-  livereload.listen();
+gulp.task('watch', ['copyAll', 'copyImages'], function() {
   gulp.watch(paths.angularFiles, ['scripts']);
   gulp.watch(paths.bowerSrc, ['vendors']);
   gulp.watch(paths.appSrc, ['copyAll']);
   gulp.watch(paths.index, ['copyAll']);
 });
 
-gulp.task('copyImages', function(){
+gulp.task('copyImages', function() {
   return gulp.src(paths.jqxImages).pipe(gulp.dest(paths.tempVendorImages));
 });
 
-gulp.task('copyAll', function(){
+gulp.task('copyAll', function() {
   var tempVendors = gulp.src(mainBowerFiles()).pipe(gulp.dest(paths.tempVendor));
 
   var appFiles = gulp.src(paths.appSrc).pipe(gulp.dest(paths.temp));
-  
+
   var angularFiles = gulp.src(paths.angularFiles).pipe(angularFilesort()).pipe(gulp.dest(paths.temp));
-  
+
   return gulp.src(paths.index)
     .pipe(gulp.dest(paths.temp))
     .pipe(inject(tempVendors, {
@@ -52,48 +52,59 @@ gulp.task('copyAll', function(){
     .pipe(inject(appFiles, {
       relative: true
     }))
-    .pipe(inject(angularFiles,{
+    .pipe(inject(angularFiles, {
       relative: true
     }))
-    .pipe(gulp.dest(paths.temp))
-    .pipe(livereload());
-  
+    .pipe(gulp.dest(paths.temp));
+
 });
 
-gulp.task('vendors', function(){
-  
+gulp.task('vendors', function() {
+
   var tempVendors = gulp.src(mainBowerFiles()).pipe(gulp.dest(paths.tempVendor));
-  
+
   return gulp.src(paths.tempIndex)
-    .pipe(inject(tempVendors,{
+    .pipe(inject(tempVendors, {
       relative: true,
       name: 'vendorInject'
     }))
-    .pipe(gulp.dest(paths.temp))
-    .pipe(livereload());
+    .pipe(gulp.dest(paths.temp));
 });
 
 
-gulp.task('scripts', function () {
+gulp.task('scripts', function() {
 
   var appFiles = gulp.src(paths.appSrc).pipe(gulp.dest(paths.temp));
-  
+
   var angularFiles = gulp.src(paths.angularFiles).pipe(angularFilesort()).pipe(gulp.dest(paths.temp));
 
   return gulp.src(paths.tempIndex)
     .pipe(inject(appFiles, {
       relative: true
     }))
-    .pipe(inject(angularFiles,{
+    .pipe(inject(angularFiles, {
       relative: true
     }))
-    .pipe(gulp.dest(paths.temp))
-    .pipe(livereload());
+    .pipe(gulp.dest(paths.temp));
 });
 
-gulp.task('clean', function(){
+gulp.task('serve', function() {
+  gulp.src(paths.temp)
+    .pipe(webserver({
+      livereload: true,
+      proxies: [
+        {
+          source: '/user',
+          target: 'http://localhost:1337/user'
+        },
+        {
+          source: '/api',
+          target: 'http://localhost:1337/api'
+        },
+      ]
+    }));
+});
+
+gulp.task('clean', function() {
   del([paths.temp]);
 });
-
-
-
