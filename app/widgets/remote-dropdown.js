74898af19e8dd981f9042077a6f3cb0789f771af
dmitriy.ivanovskiy <dmitriy.ivanovskiy@reisystems.com>
2016-05-12 16:39:16 -0400

    fixed the issues with the dropdown not functioning

--- app/widgets/remote-dropdown.js
+++ app/widgets/remote-dropdown.js
@@ -14,21 +14,31 @@
       link: function(scope, element, attrs, controller) {
         var columnName = attrs.column;
         angular.element(document).ready(function() {
-          $('[column=' + columnName + ']')
-            .dropdown({
-              apiSettings: {
-                onResponse: function(apiResponse) {
-                  var response = {
-                    results: []
-                  };
-                  $.each(apiResponse.rows, function(index, item) {
-                    response.results.push({ "name": item, "value": item });
-                  });
-                  return response;
-                },
-                url: '/api/search/query?sql=SELECT+' + columnName + '+FROM+contract+WHERE+' + columnName + '+like+' + '\'{query}%25\'+GROUP+BY+' + columnName
-              }
-            });
+
+          $('[column=' + columnName + ']').dropdown();
+
+          setTimeout(function(){
+
+            $('[column=' + columnName + ']').parent()
+                .dropdown({
+                  apiSettings: {
+                    onResponse: function(apiResponse) {
+                      var response = {
+                        results: []
+                      };
+                      $.each(apiResponse.rows, function(index, item) {
+                        response.results.push({ "name": item, "value": item });
+                      });
+                      console.log('Response' + response);
+                      return response;
+                    },
+                    url: '/api/search/query?sql=SELECT+' + columnName + '+FROM+contract+WHERE+' + columnName + '+like+' + '\'{query}%25\'+GROUP+BY+' + columnName
+                  }
+                });
+
+          }, 1000);
+
+
         });
 
       }
