6372316ead08337f5d5de9053713b9dd19bb44b1
anurag.mundada <anurag.mundada@reisystems.com>
2016-05-12 10:52:49 -0400

    no changes

--- app/search/search.js
+++ app/search/search.js
@@ -34,12 +34,6 @@
       $scope.$broadcast('updateTable', sqlClause);
     }
 
-   $scope.labels = ['2006', '2007', '2008', '2009', '2010', '2011', '2012'];
-    $scope.series = ['Series A'];
-
-  $scope.data = [
-    [65, 59, 80, 81, 56, 55, 40]
-  ];
 
 
   }
