96ebef70584a91f114e173b6f3b789e6c705e3d7
anurag.mundada <anurag.mundada@reisystems.com>
2016-05-26 18:03:02 -0400

    modified css

--- app/widgets/search-bar.js
+++ app/widgets/search-bar.js
@@ -1,30 +1,36 @@
 (function() {
-  'use strict';
+    'use strict';
 
-  angular.module('app.widgets')
-    .directive('searchBar', function() {
-      return {
-        restrict: 'E',
-        replace: true,
-        templateUrl: 'widgets/search-bar.html',
-        link: function(scope, element, attrs, controller, $location ) {
-          angular.element(document).ready(function() {
-            $('.ui.search')
-              .search({
-                apiSettings: {
-                  url: 'api/search/category?q={query}'
-                },
-                type: 'category',
-                onSelect: function(result, response) {
-                    $(".prompt").attr('disabled', true);
-                    window.location= '/#/search'
-                       var sqlClause = 'WHERE ' + result.column + "=" + "'" + result.title + "'";
-                       scope.vm.updateTableResults(sqlClause);
+    angular.module('app.widgets')
+        .directive('searchBar', function() {
+            return {
+                restrict: 'E',
+                replace: true,
+                templateUrl: 'widgets/search-bar.html',
+                link: function(scope, element, attrs, controller, $location ) {
+                    angular.element(document).ready(function() {
+                        $('.ui.search')
+                            .search({
+                                apiSettings: {
+                                    url: 'api/search/category?q={query}'
+                                },
+                                fields: {
+                                    results : 'results',
+                                    title   : 'title',
+                                    description: 'piid',
+                                    price: 'M'
+                                },
+                                debug: true,
+                                type: 'category',
+                                onSelect: function(result, response) {
+                                    window.location= '/#/search'
+                                    var sqlClause = 'WHERE ' + result.column + "=" + "'" + result.title + "'";
+                                    scope.vm.updateTableResults(sqlClause);
+                                }
+                            });
+                    });
                 }
-              });
-          });
-        }
-      };
-    });
+            };
+        });
 
 })();
